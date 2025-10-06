ff4d30996c766c87400ef07ceb90795155141b68
Éderson Fernandes <efernandes.tech@gmail.com>
2025-10-05 23:10:20 -0300

    Add TodoRepository interface

---
+++ backend/todo/src/main/java/com/example/todo/repository/TodoRepository.java
@@ -0,0 +1,9 @@
+package com.example.todo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import main.java.com.example.todo.model.Todo;
+
+public interface TodoRepository extends JpaRepository<Todo, Long> {
+    // That's it! CRUD methods are inherited
+}
